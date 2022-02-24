e00bb5c7dce5d3e330fdf3c052dde467ea90ecfd
uwePhillPhelps <philip3.phelps@uwe.ac.uk>
2022-02-24 11:24:22 +0000

    updated and tidied aserve OSC messaging header

--- iapProj/Source/Aserve.h
+++ iapProj/Source/Aserve.h
@@ -14,9 +14,8 @@
  */
 namespace AserveOSC
 {
-    
     static const String aserve = "/aserve/";
-    
+  
     static const String note = aserve + "note";
     static const String control = aserve + "control";
     static const String mod = aserve + "modwheel";
@@ -24,27 +23,33 @@ namespace AserveOSC
     static const String aftertouch = aserve + "aftertouch";
     static const String pressure = aserve + "channelpressure";
     static const String MIDI = aserve + "midi";
-    
+
     static const String oscilator = aserve + "osc";
-    static const String sample = aserve + "sample";
-    static const String pitchedSample = aserve + "samplepitch";
+    static const String pan = aserve + "pan";       // oscillator pan
+    static const String mode = aserve + "mode";     // oscillator mode
+  
     static const String setPixelGrid = aserve + "pixelgrid";
     static const String pixelGridClicked = aserve + "clickedpixelgrid";
+  
+    // samples
     static const String loadsample = aserve + "loadsample";
     static const String loadPitchedSample = aserve + "loadpitchedsample";
+    static const String loadDefaultSounds = aserve + "loaddefaults";
+    static const String sample = aserve + "sample";     // play oneshot sample
+    static const String pitchedSample = aserve + "samplepitch";
+  
+    // master bus filters
     static const String lpf = aserve + "lpf";
     static const String hpf = aserve + "hpf";
     static const String bpf = aserve + "bpf";
     static const String brf = aserve + "brf";
-    
-    static const String loadDefaultSounds = aserve + "loaddefaults";
-    static const String reset = aserve + "reset";
-    static const String mode = aserve + "mode";
-    static const String pan = aserve + "pan";
-    static const String reg = aserve + "register";
-    
-    static const String fPath = aserve + "path";
-
+  
+    static const String reset = aserve + "reset";       // reset aserve state
+  
+    static const String reg = aserve + "register";      // removed in Aserve 2.2.1
+  
+    static const String fPath = aserve + "path";        // iapProj path
+    static const String unitTest = aserve + "unittest"; // begin test
 }
 
 
