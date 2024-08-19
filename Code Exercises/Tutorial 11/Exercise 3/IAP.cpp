3147b5c70c13d402f3b422330f9bcf85708757d7
uwePhillPhelps <philip3.phelps@uwe.ac.uk>
2024-08-19 10:46:56 +0100

    building a music sequencer

--- Code Exercises/Tutorial 11/Exercise 3/IAP.cpp
+++ Code Exercises/Tutorial 11/Exercise 3/IAP.cpp
@@ -16,19 +16,19 @@ void IAP::run ()
 
     // these three lines setup our 'default' sequence
     // remove these if you want the program to start silent
-    notes.push_back( 60 );
-    notes.push_back( 64 );
-    notes.push_back( 67 );
+    noteVector.push_back( 60 );
+    noteVector.push_back( 64 );
+    noteVector.push_back( 67 );
 
     playMode = true; // begin in play mode
     while (true) 
     {
-        std::cout << "There are " << notes.size() << " notes in the sequence\n";
+        std::cout << "There are " << noteVector.size() << " notes in the sequence\n";
         if( playMode )
         {
-            for(int i=0; i<notes.size(); i++) // process each note, in order
+            for(int i=0; i<noteVector.size(); i++) // process each note, in order
             {
-                int note = notes[i];                            // get note at index 'i'
+                int note = noteVector[i];                            // get note at index 'i'
                 float frequency = 440 * pow(2, (note-69)/12.0); // calculate current note frequency
                 float amplitude = 0.5;                          // fixed amplitude
                 aserveOscillator(0, frequency, amplitude, 0);   // let's hear the current note
@@ -46,5 +46,5 @@ void IAP::callbackCCValueChanged (int cc, int value)
 
 void IAP::callbackNoteReceived  (int note, int velocity, int channel)
 {
-    notes.push_back( note ); // record notes into the sequence
+    noteVector.push_back( note ); // record received notes into the sequence
 }
