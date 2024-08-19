ce179f3ce61d3e47c57233f9050e811bd90aeb21
uwePhillPhelps <philip3.phelps@uwe.ac.uk>
2024-08-19 13:37:50 +0100

    added introductory demo code to iapProj and iapProjM

--- iapProj/Source/IAP.h
+++ iapProj/Source/IAP.h
@@ -1,42 +1,180 @@
-//
 //  IAP.h
-//  IAPProjectDevelopmentTest1
-//
-//  Created by Samuel Hunt on 16/07/2018.
-//  Copyright (c) 2018 SJHDevelopment. All rights reserved.
-//
+//  Last updated 2024 aug 19
 
 #ifndef __IAPProjectDevelopmentTest1__IAP__
 #define __IAPProjectDevelopmentTest1__IAP__
 
 #include "AserveComs.h"
+#include <unordered_map>
 
 //---------------------------------------------------------------------------------
 // USER CREATED CLASS
 
+class IAPMusicTools : public AserveComs
+{
+public:
+    std::array<int, 7> currentPitchClassSet;
+    std::string currentKeySignature = "G Minor";
+    bool isMajor = false;
+    bool isChordMode = false;
+    
+    std::unordered_map<std::string, int> KeySignatures = {
+            {"Ab Major", 0},
+            {"A Major", 1},
+            {"A# Major", 2},
+            {"Bb Major", 3},
+            {"B Major", 4},
+            {"C Major", 5},
+            {"C# Major", 6},
+            {"Db Major", 7},
+            {"D Major", 8},
+            {"D# Major", 9},
+            {"Eb Major", 10},
+            {"E Major", 11},
+            {"F Major", 12},
+            {"F# Major", 13},
+            {"Gb Major", 14},
+            {"G Major", 15},
+            {"G# Major", 16},
+            {"Ab Minor", 17},
+            {"A Minor", 18},
+            {"A# Minor", 19},
+            {"Bb Minor", 20},
+            {"B Minor", 21},
+            {"C Minor", 22},
+            {"C# Minor", 23},
+            {"Db Minor", 24},
+            {"D Minor", 25},
+            {"D# Minor", 26},
+            {"Eb Minor", 27},
+            {"E Minor", 28},
+            {"F Minor", 29},
+            {"F# Minor", 30},
+            {"Gb Minor", 31},
+            {"G Minor", 32},
+            {"G# Minor", 33}
+        };
+   
+    std::unordered_map<std::string, std::array<int, 7>> majorScales = {
+        {"Ab Major", {8, 10, 0, 1, 3, 5, 7}},
+        {"A Major",  {9, 11, 1, 2, 4, 6, 8}},
+        {"Bb Major", {10, 0, 2, 3, 5, 7, 9}},
+        {"B Major",  {11, 1, 3, 4, 6, 8, 10}},
+        {"C Major",  {0, 2, 4, 5, 7, 9, 11}},
+        {"C# Major", {1, 3, 5, 6, 8, 9, 0 }},
+        {"D Major",  {2, 4, 6, 7, 9, 11, 1}},
+        {"Eb Major", {3, 5, 7, 8, 10, 0, 2}},
+        {"E Major",  {4, 6, 8, 9, 11, 1, 3}},
+        {"F Major",  {5, 7, 9, 10, 0, 2, 4}},
+        {"F# Major", {6, 8, 10, 11, 1, 4, 5}},
+        {"G Major",  {7, 9, 11, 0, 2, 4, 6}},
+    };
+    
+    std::unordered_map<std::string, std::array<int, 7>> minorScales = {
+           {"C Minor",  {0, 2,  3,  5, 7, 8, 10}},
+           {"C# Minor", {1, 3,  4,  6, 8, 9, 11}},
+           {"D Minor",  {2, 4,  5,  7, 9, 10, 0}},
+           {"Eb Minor", {3, 5,  6,  8, 10, 11, 1}},
+           {"E Minor",  {4, 6,  7,  9, 11, 0, 2}},
+           {"F Minor",  {5, 7,  8,  10, 0, 1, 3}},
+           {"F# Minor", {6, 8,  9,  11, 1, 2, 4}},
+           {"G Minor",  {7, 9,  10, 0, 2, 3, 5}},
+           {"Ab Minor", {8, 10, 11, 1, 3, 4, 6}},
+           {"A Minor",  {9, 11, 0,  2, 4, 5, 7}},
+           {"Bb Minor", {10, 0, 1,  3, 5, 6, 8}},
+           {"B Minor",  {11, 1, 2,  4, 6, 7, 9}},
+       };
+    
+    void getPitchClassesOfKey(std::string _currentKeySignature)
+    {
+        int index = KeySignatures.at(_currentKeySignature);
+        if(index <= 16)
+        {
+            currentPitchClassSet = majorScales.at(_currentKeySignature);
+            isMajor = true;
+        }
+        else
+        {
+            currentPitchClassSet = minorScales.at(_currentKeySignature);
+            isMajor = false;
+        }
+        
+        std::cout << "Your chosen scale is: " << currentKeySignature << "\n";
+        
+    }
+    
+    int transposeNotesToPitchClass(int note)
+    {
+        
+    int pitchClass = note % 12;
+    bool foundNote = false;
+    
+    for(int i = 0; i < currentPitchClassSet.size(); i++)
+    {
+        if(currentPitchClassSet[i] == pitchClass)
+        {
+            foundNote = true;
+            break;
+        }
+    }
+    
+    if(!foundNote) note++;
+    
+    return note;
+}
+    
+    float mtof(int note){
+    return 440.0 * (pow(2, (note-69.0) / 12.0));
+    
+}
+    
+    void changeKeySignature()
+    {
+        std::cout << "Please enter a new Key Signature in the format 'KEY <space> Tonality' \n";
+        std::getline(std::cin, currentKeySignature);
+        std::cout << "Key Signature updated to " << currentKeySignature << "\n";
+        getPitchClassesOfKey(currentKeySignature);
+
+    }
+    
+    void toggleChordMode()
+    {
+        if(isChordMode) { isChordMode = false; std::cout << "Chord Mode Activated\n"; }
+            else        { isChordMode = true;  std::cout << "Chord Mode Activated\n"; }
+        
+    }
+  
+};
 
 class IAP : public AserveComs  {
 public:
     
     //---------------------------------------------------------------------------------
     // SHARED VARIABLES (IAP member variables)
+    IAPMusicTools musicTools;
     
-
     //---------------------------------------------------------------------------------
     // FUNCTIONS (IAP class methods)
     void run ();
-
     //---------------------------------------------------------------------------------
     // CALLBACK FUNCTIONS
-    
-    //void callbackNoteReceived  (int note, int velocity, int channel);
-    //void callbackModWheelMoved (int value);
+    void callbackNoteReceived  (int note, int velocity, int channel);
+    // void callbackModWheelMoved (int value);
     //void callbackPitchbendWheelMoved (int value);
-    //void callbackCCValueChanged (int cc, int value);
+    void callbackCCValueChanged (int cc, int value);
+    
+    float BPMToMS(int bpm)
+    {
+        if (bpm > 10000) bpm = 10000;
+        if (bpm < 1 ) bpm = 1;
+        
+        return 60000.0 / bpm;
+        
+    }
     
     //void callbackMIDIReceived (MIDI message);
     //void callbackPixelGrid (int x, int y);
-    
+  
 private:
     
 };
--- iapProjM/Source/IAP.cpp
+++ iapProjM/Source/IAP.cpp
@@ -1,19 +1,217 @@
-//
 //  IAP.cpp
-//  IAPProjectDevelopmentTest1
-//
-//  Created by Samuel Hunt on 16/07/2018.
-//  Copyright (c) 2018 SJHDevelopment. All rights reserved.
-//
+//  Last updated 2024 aug 19
 
 #include "IAP.h"
 #include <iostream>
 
+/* **** **** **** **** **** **** **** **** **** **** **** **** **** **** **** **** ****
+
+ Welcome to the Introductory Audio Programming demo!
+ Here we have developed a synthesiser and sample playback system for you to experiment with.
+ Below are a series of VARIABLES you can change to affect how the system works.
+ You can do this by altering the relevant piece of code. For example:
+ 
+ #define BPM 180
+ #define BPM 120
+ #define BPM 1000 (the skys the limit!)
+ 
+ #define USE_DRUMS true
+ #define USE_DRUMS false
+ 
+ #define USE_CHORDS true
+ #define USE_CHORDS false
+ 
+ #define USE_HIGH_OCTAVE true
+ #define USE_HIGH_OCTAVE false
+ 
+ #define USE_LOW_OCTAVE true
+ #define USE_LOW_OCTAVE false
+
+ std::string keySignature = "E Minor";
+ std::string keySignature = "B Major";
+ std::string keySignature = "B Careful to use the correct formatting!"; (This would not work...)
+
+ **** **** **** **** **** **** **** **** **** **** **** **** **** **** **** **** **** */
+
+#define BPM 300
+
+#define USE_DRUMS true
+#define USE_CHORDS true
+
+#define USE_HIGH_OCTAVE true
+#define HIGH_OCTAVE_MOD 12
+
+#define USE_LOW_OCTAVE true
+#define LOW_OCTAVE_MOD -12
+
+std::string keySignature = "C Minor";
+
+/* **** **** **** **** **** **** **** **** **** **** **** **** **** **** **** **** ****
+
+ You should also have access to a series of audio samples.
+ Please make a folder on your desktop called 'sounds' and place 4 samples in it.
+ You should name these samples 1, 2, 3, and 4:
+ 
+ 1.wav
+ 2.wav
+ 3.wav
+ 4.wav
+ 
+ These samples will play sequentially at crotchet rhythm based on your BPM above.
+ If you have additional samples to the ones provided, please use them too!
+*/
+
+/* **** **** **** **** **** **** **** **** **** **** **** **** **** **** **** **** ****
+ 
+ Below here is the rest of the code that makes our project work!
+ We'll get to this throughout the course of IAP so venture at your own peril.
+ 
+ It it crucial to note here that the code might look scary and overwhelming at first.
+ Through slow, bitesize practicals and awesome results, this will become familiar in no time :D
+ 
+**** **** **** **** **** **** **** **** **** **** **** **** **** **** **** **** **** */
 
 void IAP::run ()
 {
-    std::cout << "Hello! \n";
+    aserveLoadDefaultSounds();
+    musicTools.currentKeySignature = keySignature;
+    musicTools.getPitchClassesOfKey(musicTools.currentKeySignature);
+  
+  
+    while(true)
+    {
+        if(USE_DRUMS)
+        {
+            aservePlaySample(0, 1.0);
+            aserveSleep( BPMToMS(BPM));
+            aservePlaySample(1, 1.0);
+            aserveSleep( BPMToMS(BPM));
+            aservePlaySample(2, 1.0);
+            aserveSleep( BPMToMS(BPM));
+            aservePlaySample(3, 1.0);
+            aserveSleep( BPMToMS(BPM));
+        }
+        else
+        {
+            aserveSleep(BPMToMS(BPM*4));
+        }
+      
+      
+    }
 }
+
+void IAP::callbackNoteReceived(int note, int velocity, int channel)
+{
+    if(velocity > 0) //Note on message received
+    {
+      
+        //if(musicTools.isChordMode)
+        if (USE_CHORDS)
+        {
+            if(musicTools.isMajor)
+            {
+                int transposedNoteRoot = musicTools.transposeNotesToPitchClass(note);
+                int transposedNoteThird = musicTools.transposeNotesToPitchClass(note + 4);
+                int transposedNoteFifth = musicTools.transposeNotesToPitchClass(note + 7);
+              
+                aserveOscillator(0, musicTools.mtof(transposedNoteRoot), 0.5, 1);
+                if(USE_LOW_OCTAVE) aserveOscillator(3, musicTools.mtof(transposedNoteRoot - LOW_OCTAVE_MOD), 0.25, 1);
+                if(USE_HIGH_OCTAVE)aserveOscillator(4, musicTools.mtof(transposedNoteRoot + HIGH_OCTAVE_MOD), 0.25, 1);
+
+                aserveOscillator(1, musicTools.mtof(transposedNoteThird), 0.5, 1);
+                if(USE_LOW_OCTAVE) aserveOscillator(5, musicTools.mtof(transposedNoteThird + LOW_OCTAVE_MOD), 0.25, 1);
+                if(USE_HIGH_OCTAVE)aserveOscillator(6, musicTools.mtof(transposedNoteThird + HIGH_OCTAVE_MOD), 0.25, 1);
+
+                aserveOscillator(2, musicTools.mtof(transposedNoteFifth), 0.5, 1);
+                if(USE_HIGH_OCTAVE) aserveOscillator(7, musicTools.mtof(transposedNoteFifth + HIGH_OCTAVE_MOD), 0.25, 1);
+                if(USE_LOW_OCTAVE)aserveOscillator(8, musicTools.mtof(transposedNoteFifth - LOW_OCTAVE_MOD), 0.25, 1);
+
+
+            }
+            else
+            {
+                int transposedNoteRoot = musicTools.transposeNotesToPitchClass(note);
+                int transposedNoteThird = musicTools.transposeNotesToPitchClass(note + 3);
+                int transposedNoteFifth = musicTools.transposeNotesToPitchClass(note + 7);
+              
+                aserveOscillator(0, musicTools.mtof(transposedNoteRoot), 0.5, 1);
+                if(USE_LOW_OCTAVE) aserveOscillator(3, musicTools.mtof(transposedNoteRoot - LOW_OCTAVE_MOD), 0.25, 1);
+                if(USE_HIGH_OCTAVE)aserveOscillator(4, musicTools.mtof(transposedNoteRoot + HIGH_OCTAVE_MOD), 0.25, 1);
+
+                aserveOscillator(1, musicTools.mtof(transposedNoteThird), 0.5, 1);
+                if(USE_LOW_OCTAVE) aserveOscillator(5, musicTools.mtof(transposedNoteThird + LOW_OCTAVE_MOD), 0.25, 1);
+                if(USE_HIGH_OCTAVE)aserveOscillator(6, musicTools.mtof(transposedNoteThird + HIGH_OCTAVE_MOD), 0.25, 1);
+
+                aserveOscillator(2, musicTools.mtof(transposedNoteFifth), 0.5, 1);
+                if(USE_HIGH_OCTAVE) aserveOscillator(7, musicTools.mtof(transposedNoteFifth + HIGH_OCTAVE_MOD), 0.25, 1);
+                if(USE_LOW_OCTAVE)aserveOscillator(8, musicTools.mtof(transposedNoteFifth - LOW_OCTAVE_MOD), 0.25, 1);
+
+            }
+        }
+        else
+        {
+            int transposedNote = musicTools.transposeNotesToPitchClass(note);
+            aserveOscillator(0, musicTools.mtof(transposedNote), 0.5, 1);
+            if(USE_HIGH_OCTAVE) aserveOscillator(3, musicTools.mtof(transposedNote + HIGH_OCTAVE_MOD), 0.25, 1);
+            if(USE_LOW_OCTAVE)aserveOscillator(4, musicTools.mtof(transposedNote - LOW_OCTAVE_MOD), 0.25, 1);
+
+          
+
+        }
+
+      
+     
+    
+    }
+    else //Note off message received
+    {
+        //if(musicTools.isChordMode)
+      
+        aserveOscillator(0, 0, 0, 1);
+
+        if(USE_CHORDS)
+        {
+            aserveOscillator(1, 0, 0, 1);
+            aserveOscillator(2, 0, 0, 1);
+       
+        }
+      
+        if(USE_HIGH_OCTAVE)
+        {
+            aserveOscillator(4, 0, 0, 0);
+            aserveOscillator(6, 0, 0, 0);
+            aserveOscillator(8, 0, 0, 0);
+        }
+      
+        if (USE_LOW_OCTAVE)
+        {
+            aserveOscillator(3, 0, 0, 0);
+            aserveOscillator(5, 0, 0, 0);
+            aserveOscillator(7, 0, 0, 0);
+        }
+      
+      
+      
+    }
+}
+
+void IAP::callbackCCValueChanged(int cc, int value)
+{
+    if(cc == 51)
+    {
+        musicTools.changeKeySignature();
+    }
+  
+    if(cc == 52)
+    {
+        musicTools.toggleChordMode();
+    }
+}
+
+
+
+
+
 
 
 
