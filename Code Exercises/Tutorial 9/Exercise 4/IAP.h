7ae887d100229d5e4186d2677f403fb161c4a045
uwePhillPhelps <philip3.phelps@uwe.ac.uk>
2024-08-20 17:14:28 +0100

    update starting point for Tutorial 9 IAPSynth

--- Code Exercises/Tutorial 9/Exercise 4/IAP.h
+++ Code Exercises/Tutorial 9/Exercise 4/IAP.h
@@ -1,9 +1,5 @@
 //
 //  IAP.h
-//  IAPProjectDevelopmentTest1
-//
-//  Created by Samuel Hunt on 16/07/2018.
-//  Copyright (c) 2018 SJHDevelopment. All rights reserved.
 //
 
 #ifndef __IAPProjectDevelopmentTest1__IAP__
@@ -11,34 +7,49 @@
 
 #include "AserveComs.h"
 
-//---------------------------------------------------------------------------------
+//---------------------------------------------------------------------
 // USER CREATED CLASS
-
+// You might add your own classes, like 'IAPSynth' or 'Person'
+class IAPSynth : public AserveComs  {
+public:
+    //---------------------------------------------------------------------
+    // SHARED VARIABLES (member variables)
+    // You might add your own variables, like 'wave' or 'currentNote'
+  
+    //---------------------------------------------------------------------
+    // FUNCTIONS (class methods)
+    // You might add your own functions, like 'mtof()' or 'playNote()'
+    void introduceYourself()
+    {
+        std::cout << "Hi, I'm an IAPSynth \n";
+        std::cout << "I don't make noise yet... but I will soon! \n";
+    }
+};
 
 class IAP : public AserveComs  {
 public:
-    
-    //---------------------------------------------------------------------------------
-    // SHARED VARIABLES
-
-    //---------------------------------------------------------------------------------
-    // FUNCTIONS
+  
+    //---------------------------------------------------------------------
+    // SHARED VARIABLES (IAP member variables)
+    // You might add your own variables, like 'wave' or 'currentNote'
+    IAPSynth synth;
+  
+    //---------------------------------------------------------------------
+    // FUNCTIONS (IAP class methods)
+    // You might add your own functions, like 'mtof()' or 'minorChord()'
     void run ();
   
-
-    //---------------------------------------------------------------------------------
+    //---------------------------------------------------------------------
     // CALLBACK FUNCTIONS
-    
     void callbackNoteReceived  (int note, int velocity, int channel);
+    void callbackCCValueChanged (int cc, int value);
     //void callbackModWheelMoved (int value);
     //void callbackPitchbendWheelMoved (int value);
-    void callbackCCValueChanged (int cc, int value);
-    
-    //void callbackMIDIRecived (MIDI message);
+    //void callbackMIDIReceived (MIDI message);
     //void callbackPixelGrid (int x, int y);
-    
+
 private:
-    
+
 };
 
 #endif /* defined(__IAPProjectDevelopmentTest1__IAP__) */
