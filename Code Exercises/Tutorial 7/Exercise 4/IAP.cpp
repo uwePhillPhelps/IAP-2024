f47f8e4d88cca50467dbf71cc6e39d97cd161e0a
uwePhillPhelps <philip3.phelps@uwe.ac.uk>
2024-08-28 10:40:34 +0100

    scale quantisation exercise

--- Code Exercises/Tutorial 7/Exercise 4/IAP.cpp
+++ Code Exercises/Tutorial 7/Exercise 4/IAP.cpp
@@ -1,33 +1,36 @@
 //
 //  IAP.cpp
-//  IAPProjectDevelopmentTest1
-//
-//  Created by Samuel Hunt on 16/07/2018.
-//  Copyright (c) 2018 SJHDevelopment. All rights reserved.
 //
 
 #include "IAP.h"
 #include <iostream>
 
-// This is our main function code.
-
 void IAP::run ()
 {
-    while (true) {
+    while (true)
+    {
         aserveSleep(1000);
     }
-    
 }
+
 void IAP::callbackNoteReceived  (int note, int velocity, int channel)
 {
-    
-    float frequency = 440.0 * pow(2.0, (note-69)/12.0);
-    
-    if (velocity > 0) {
+    float frequency = 440.0 * pow(2.0, (note-69)/12.0);  // calculate frequency
+  
+    if (velocity > 0)
+    {
         aserveOscillator(0, frequency, 1.0, 0);
     }
-    else {
+    else
+    {
         aserveOscillator(0, 0, 0, 0);
     }
 }
 
+void IAP::callbackCCValueChanged (int cc, int value)
+{
+    // in the later parts of this exercise...
+    // ...you might select between multiple possible scales like this
+    //if (cc == 51 ) { desiredScale = 0; }
+    //if (cc == 52 ) { desiredScale = 1; }
+}
