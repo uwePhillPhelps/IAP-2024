0df283c3897553d5ca7ab77372dd2260646a234d
uwePhillPhelps <philip3.phelps@uwe.ac.uk>
2024-09-12 11:16:43 +0100

    a clean IAP.cpp starting point

--- Code Exercises/Tutorial 1/IAP.cpp
+++ Code Exercises/Tutorial 1/IAP.cpp
@@ -5,7 +5,7 @@
 #include "IAP.h"
 #include <iostream>
 
-// This is our main function code. Nearly Everything goes in here..
+/* This is our main function code. Nearly Everything goes in here. */
 void IAP::run ()
 {
     std::cout << "Hello world! \n";
