154c2bc42eb210aea1d5fca2b071caee3c62e35b
uwePhillPhelps <philip3.phelps@uwe.ac.uk>
2024-09-18 11:17:26 +0100

    Update IAP.cpp

--- Code Exercises/Tutorial 4/Exercise 3/IAP.cpp
+++ Code Exercises/Tutorial 4/Exercise 3/IAP.cpp
@@ -1,16 +1,11 @@
 //
 //  IAP.cpp
-//  IAPProjectDevelopmentTest1
-//
-//  Created by Samuel Hunt on 16/07/2018.
-//  Copyright (c) 2018 SJHDevelopment. All rights reserved.
 //
 
 #include "IAP.h"
 #include <iostream>
 
-// This is our main function code. Nearly Everything goes in here..
-
+/* This is our main function code. Nearly Everything goes in here. */
 void IAP::run ()
 {
     aserveLoadDefaultSounds();
@@ -19,8 +14,11 @@ void IAP::run ()
     }
 }
 
-
 void IAP::callbackNoteReceived  (int note, int velocity, int channel)
 {
-    
+    std::cout << "Received " << note << "\n";
+
+    if( note == 60 ) std::cout << "Boing\n";
+    if( note == 61 ) std::cout << "Boom\n";
+    if( note == 62 ) std::cout << "Tschak\n";
 }
